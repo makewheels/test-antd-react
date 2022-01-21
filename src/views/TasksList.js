9b6063b8ba7f3aff8f08a02a9c95500e6a638b53
binqiao <you@example.com>
2022-01-21 17:56:43 +0800

    test

---
+++ src/views/TasksList.js
@@ -0,0 +1,11 @@
+import React,{ Component } from "react";
+
+import { Button } from "antd";
+import "antd/dist/antd.css"
+
+class TasksList extends Component{
+    render(){
+        return <div>hello world</div>;
+    }
+}
+export default TasksList;
